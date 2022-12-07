62a2b11c7efd039ac00d37ffa02f8b6991abf0a2
Nikos-I <109852894+Nikos-I@users.noreply.github.com>
2022-12-07 15:29:16 +0300

    seminar1 ДЗ2 1,2 завершено

--- src/org/geekbrains/Hw2.java
+++ src/org/geekbrains/Hw2.java
@@ -1,11 +1,9 @@
 package org.geekbrains;
 
-import java.util.ArrayList;
-
 public class Hw2 {
     public int[] buildArray(int[] nums) {
         int[] ret = new int[nums.length];
-        for (int i = 0; i < nums.length ; i++) {
+        for (int i = 0; i < nums.length; i++) {
             ret[i] = nums[nums[i]];
         }
         return ret;
